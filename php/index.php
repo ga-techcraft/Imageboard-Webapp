c7dd6a020a655d1b76df7a894d94c9a0bed838d2
Tech Craft <ga.techcraft@gmail.com>
2025-04-13 14:05:18 +0900

    build: Set up Docker environment

---
+++ php/index.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "mysql";
+$username = "user";
+$password = "userpassword";
+$dbname = "testdb";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+}
+catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
